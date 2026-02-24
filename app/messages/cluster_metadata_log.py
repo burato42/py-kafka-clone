bf371c260211b22f46fbaea7755aa2a3ce3fca30
burato42 <tmahmutov@gmail.com>
2026-02-24 20:37:11 +0100

    Data model for ClusterMetadataLogFile and parsing logic

--- app/messages/cluster_metadata_log.py
+++ app/messages/cluster_metadata_log.py
@@ -1,11 +1,70 @@
 from dataclasses import dataclass
 
 from app.connection import Buffer
+from app.protocol import bytes_to_int
+
 
 def read_cluster_metadata_log(buffer: Buffer) -> ClusterMetadataLogFile:
-    raise NotImplementedError
+    base_offset_raw = buffer.read_bytes(8)
+    batch_length_raw = buffer.read_bytes(4)
+    partition_leader_epoch_raw = buffer.read_bytes(4)
+    magic_byte_raw = buffer.read_bytes(1)
+    crc_raw = buffer.read_bytes(4)
+    attributes_raw = buffer.read_bytes(2)
+    last_offset_delta_raw = buffer.read_bytes(4)
+    base_timestamp_raw = buffer.read_bytes(8)
+    max_timestamp_raw = buffer.read_bytes(8)
+    producer_id_raw = buffer.read_bytes(8)
+    producer_epoch_raw = buffer.read_bytes(2)
+    base_sequence_raw = buffer.read_bytes(4)
+    record_length_raw = buffer.read_bytes(4)
+    record_length = bytes_to_int(record_length_raw)
+    for _ in range:
+        ...
 
 
 @dataclass
 class ClusterMetadataLogFile:
-    raise NotImplementedError
+    record_batches: list[RecordBatch]
+
+
+@dataclass
+class RecordBatch:
+    base_offset: bytes
+    batch_length: bytes
+    partition_leader_epoch: bytes
+    magic_byte: bytes
+    crc: bytes
+    attributes: bytes
+    last_offset_delta: bytes
+    base_timestamp: bytes
+    max_timestamp: bytes
+    producer_id: bytes
+    producer_epoch: bytes
+    base_sequence: bytes
+    # Don't forget the length during the parsing 
+    records: list[Record]
+    
+    
+@dataclass
+class Record:
+    length: bytes  
+    attributes: bytes
+    timestamp_delta: bytes
+    offset_delta: bytes
+    key_length: bytes
+    key: bytes
+    value_length:bytes
+    value: Value
+    headers_array_count: bytes
+
+
+@dataclass
+class Value:
+    frame_version: bytes
+    type: bytes
+    version: bytes
+    name_length: bytes
+    name: bytes
+    feature_level: bytes
+    tagged_fields_count: bytes
